a320766c681c92a4f65b9239035444ea2dd45a5c
Dmitry <ximoske@gmail.com>
2022-11-18 04:18:18 +0300

    "Auto" session constructor and related updates

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import Session from './lib/SteamSession.js'
 import prompt from "./lib/utils/prompt.js";
+import {
+  defaultActor, expectConfirmViaDeviceActor, expectNoGuardActor,
+  deviceCodePromptActor, emailCodePromptActor, deviceCodeActor
+} from "./lib/actors.js";
+import {webBrowser as webBrowserEnv, mobileIOS as mobileIOSEnv, clientWindows as clientWindowsEnv} from "./lib/utils/genRequestEnv.js";
+import decodeJWT from "./lib/utils/decodeJWT.js";
 
 export default Session
-export {prompt}
+export {
+  Session as SteamSession,
+  prompt, decodeJWT, webBrowserEnv, mobileIOSEnv, clientWindowsEnv, defaultActor, expectNoGuardActor,
+  emailCodePromptActor, deviceCodeActor, deviceCodePromptActor, expectConfirmViaDeviceActor
+}
